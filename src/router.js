6999f0453817686c2df0f2a170b039e22b19beee
Shreyas Purohit <shreyas.purohit@gmail.com>
2013-02-03 05:20:12 -0600

    Excluding resources from logging

--- src/router.js
+++ src/router.js
@@ -4,8 +4,7 @@ var resources = require("./resources");
 var functions = require("./functions");
 var domain = require('domain');
 
-function route(rrWrapper, handlers) {
-  common.winston.debug("Routing:" + rrWrapper.pathName + " Header[Accept-Encoding]: " + rrWrapper.requestEncodings);  
+function route(rrWrapper, handlers) {  
   var resourcePath=common.constants.resourcePathPattern + "/";  
   if(rrWrapper.pathName.search(resourcePath) === 0){
     resources.respondResource(rrWrapper.pathName, rrWrapper.response, rrWrapper.requestEncodings);
@@ -13,6 +12,7 @@ function route(rrWrapper, handlers) {
     resources.respondResource(resourcePath+"favicon.ico", rrWrapper.response, rrWrapper.requestEncodings);
   }
   else{
+	  common.winston.debug("Routing:" + rrWrapper.pathName + " Header[Accept-Encoding]: " + rrWrapper.requestEncodings);  
 	  var requestDomain = domain.create();
 	  requestDomain.on('error', function(err) {
 		  common.winston.error("Error while routing " + rrWrapper.pathName + " due to " + err);
